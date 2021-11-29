7c4f747ea3a14bb8a3934c5d274829be651fce74
Clint Ngo <clintngo@gmail.com>
2021-11-29 12:04:08 -0600

    Commit

---
+++ Clint.java
@@ -0,0 +1,7 @@
+public class Clint {
+	public static void main(String[] args) {
+		System.out.println("My name is Clint Ngo");
+		System.out.println("I am 28 years old");
+		System.out.println("My hometown is Kansas City, MO");
+	}
+}
